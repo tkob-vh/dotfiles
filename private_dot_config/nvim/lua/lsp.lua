34991ba57b8a9ebdcb69385a0ab287a5089cb7e6
yyx <yuxin.yan.cs@gmail.com>
2024-02-03 19:56:04 +0800

    Initial commit

---
+++ private_dot_config/nvim/lua/lsp.lua
@@ -0,0 +1,15 @@
+require('mason').setup({
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+})
+
+require('mason-lspconfig').setup({
+    -- A list of servers to automatically install if they're not already installed
+    ensure_installed = { 'pylsp', 'ccls', 'cmake', 'lua_ls', 'rust_analyzer' },
+})
+

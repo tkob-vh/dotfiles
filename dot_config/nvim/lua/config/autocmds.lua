b36e3a11cda1683f62bcf547dc840c6383c5c47c
tkob-vh <yuxin.yan.cs@gmail.com>
2025-05-08 13:42:17 +0800

    update nvim configs

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,11 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = false
   end,
 })
+
+-- Disable autoformat for lua files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "cpp" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -46,7 +46,7 @@ return {
           usePlaceholders = true,
           completeUnimported = true,
           clangdFileStatus = true,
-          fallbackFlags = { "--std=c++26" },
+          fallbackFlags = { "--std=c++23" },
         },
       },
     },
